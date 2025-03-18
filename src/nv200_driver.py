c72fa8d451b063042197773b1f8c47382986aed5
Uwe Kindler <uwe_kindler@web.de>
2025-03-18 13:05:06 +0100

    Implemented auto detection for SerialTransport

--- src/nv200_driver.py
+++ src/nv200_driver.py
@@ -1,28 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Ethernet_simple.py 23.12.2021 Rev0
-
-This example script shows the basic way to communicate with the NV200D/NET via 
-Telnet with known IP address. First the connection is established and it is checked if the 
-controller reacts. Then a closed loop step to a position of 10 µm (or 10mrad 
-for tilting actuators) is executed and after one second of waiting time the
-reached position of the actuator is queried and displayed.
-
-Expected output:
-NV200/D_NET>
-meas,9.942
-
-Note that the value may vary due to sensor noise and controller performance
-
-"""
-
 import asyncio, telnetlib3
 import aioserial
+import serial.tools.list_ports
 import lantronix_device_discovery_async as ldd
 from abc import ABC, abstractmethod
 
 # Abstract Base Class for Transport Protocols
 class TransportProtocol(ABC):
+    """
+    Abstract base class representing a transport protocol interface for a device.
+    """
     @abstractmethod
     async def connect(self):
         pass
@@ -42,7 +28,21 @@ class TransportProtocol(ABC):
 
 # Telnet Transport Implementation
 class TelnetTransport(TransportProtocol):
+    """
+    TelnetTransport is a class that implements a transport protocol for communicating
+    with piezosystem devices over Telnet. It provides methods to establish a connection,
+    send commands, read responses, and close the connection.
+    """
+    
     def __init__(self, host: str = None, port: int = 23, MAC: str = None):
+        """
+        Initializes thetransport protocol.
+
+        Args:
+            host (str, optional): The hostname or IP address of the NV200 device. Defaults to None.
+            port (int, optional): The port number to connect to. Defaults to 23.
+            MAC (str, optional): The MAC address of the NV200 device. Defaults to None.
+        """
         self.host = host
         self.port = port
         self.MAC = MAC
@@ -102,8 +102,29 @@ class SerialTransport(TransportProtocol):
         self.xonxoff = xonxoff
         self.baudrate = baudrate
 
+    async def detect_port(self):
+        ports = serial.tools.list_ports.comports()
+        for port in ports:
+            if port.manufacturer != "FTDI":
+                continue
+            self.serial.close()
+            self.serial.port = port.device
+            self.serial.open()
+            response = await self.read('\r')
+            if response.startswith(b"NV200"):
+                self.port = port.device
+                return self.port
+            else:
+              self.serial.close()
+            return None
+
     async def connect(self):
         self.serial = aioserial.AioSerial(port=self.port, xonxoff=self.xonxoff, baudrate=self.baudrate)
+        if self.port is None:
+            self.port = await self.detect_port()
+        if self.port is None:
+            raise RuntimeError("NV200 device not found")
+
 
     async def write(self, cmd: str):
         await self.serial.write_async(cmd.encode('utf-8'))
@@ -134,42 +155,6 @@ class DeviceClient:
         await self.transport.close()
 
 
-
-IP='192.168.10.188'     # NV200D/NET IP address, has to be adjusted to your address
-port=23                 # Telnet Port (default: 23)
-
-async def telnet_client():
-    reader, writer = await telnetlib3.open_connection(IP, port)
-    writer.write('\r')
-    response = await reader.readuntil(b'\n')
-    print(f"Server response: {response}")
-    writer.write('modsrc,0\r')   # Set setpoint source to Telnet/USB input  
-    writer.write('cl,1\r')       # Set controller to closed loop mode
-    writer.write('set,40\r')     # Set setpoint to 10 µm (or mrad for tilting actuators)
-    await asyncio.sleep(0.1)
-    writer.write('meas\r')       # Query position measurement
-    print("waiting...")
-    response = await reader.readuntil(b'\n')
-    print(f"Server response: {response}")
-    writer.close()
-
-
-async def serial_client():
-    serial = aioserial.AioSerial(port='COM3', xonxoff=True, baudrate=115200)
-    await serial.write_async(b'\r')
-    response = await serial.readline_async()
-    print(f"Server response: {response}")
-    await serial.write_async(b'modsrc,0\r')   # Set setpoint source to Telnet/USB input  
-    await serial.write_async(b'cl,1\r')       # Set controller to closed loop mode
-    await serial.write_async(b'set,40\r')     # Set setpoint to 10 µm (or mrad for tilting actuators)
-    await asyncio.sleep(0.1)
-    await serial.write_async(b'meas\r')       # Query position measurement
-    print("waiting...")
-    response = await serial.readline_async()
-    print(f"Server response: {response}")
-    serial.close()
-
-
 async def client_telnet():
     #transport = TelnetTransport(MAC="00:80:A3:79:C6:18")
     transport = TelnetTransport()
@@ -177,13 +162,13 @@ async def client_telnet():
     await client.connect()
 
     response = await client.read('')
-    print(f"Server response: {response}")    
+    print(f"Telnet - Server response: {response}")    
     await client.write('modsrc,0')
     await client.write('cl,1')
     await client.write('set,40')
     await asyncio.sleep(0.1)
     response = await client.read('meas')
-    print(f"Server response: {response}")
+    print(f"Telnet - Server response: {response}")
     await client.close()
 
 
@@ -193,36 +178,15 @@ async def client_serial():
     await client.connect()
 
     response = await client.read('')
-    print(f"Server response: {response}")    
+    print(f"Serial - Server response: {response}")    
     await client.write('modsrc,0')
     await client.write('cl,1')
     await client.write('set,40')
     await asyncio.sleep(0.1)
     response = await client.read('meas')
-    print(f"Server response: {response}")
+    print(f"Serial - Server response: {response}")
     await client.close()
 
 
-#asyncio.run(telnet_client())
-#asyncio.run(serial_client())
 asyncio.run(client_telnet())
 asyncio.run(client_serial())
-
-
-# IP='192.168.10.188'     # NV200D/NET IP address, has to be adjusted to your address
-# port=23                 # Telnet Port (default: 23)
-# timeout=0.25            # Timeout for reading device answers
-
-# NV200 = telnetlib3.Telnet(IP, port)  # open Telnet connection to NV200D/NET
-
-# NV200.write(b'\r')   # Query device prompt response 
-# print(NV200.read_until(b'\n',timeout).decode(),end='') # Read and print device prompt response
-
-# NV200.write(b'modsrc,0\r')   # Set setpoint source to Telnet/USB input
-# NV200.write(b'cl,1\r')       # Set controller to closed loop mode
-# NV200.write(b'set,40\r')     # Set setpoint to 10 µm (or mrad for tilting actuators)
-# time.sleep(0.1)                               # Wait for actuator to change position
-# NV200.write(b'meas\r')       # Query position measurement
-# print(NV200.read_until(b'\n',timeout).decode(),end='') # Read and print device answer to position measurement query
-
-# NV200.close()   # close Telnet connection to NV200D/NET

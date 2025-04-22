789e01861750f9e4f7719ce8e9c4ea6e5adbafd0
Uwe Kindler <uwe_kindler@web.de>
2025-04-22 11:28:37 +0200

    Improved SerialProtocol to speed up transfers
    - now wait for XON instead of newline because some commands do not return newline

--- nv200/transport_protocols.py
+++ nv200/transport_protocols.py
@@ -179,27 +179,23 @@ class SerialProtocol(TransportProtocol):
     Attributes:
         port (str): The serial port to connect to. Defaults to None. If port is None, the class
         will try to auto detect rthe port.
-        xonxoff (bool): Whether to enable software flow control. Defaults to True.
         baudrate (int): The baud rate for the serial connection. Defaults to 115200.
         serial (AioSerial): The AioSerial instance for asynchronous serial communication.
     """
     __port : str
-    __xonxoff : bool
     __baudrate : int
     
-    def __init__(self, port : str = None, xonxoff : bool = True, baudrate : int = 115200):
+    def __init__(self, port : str = None, baudrate : int = 115200):
         """
         Initializes the NV200 driver with the specified serial port settings.
 
         Args:
             port (str, optional): The serial port to connect to. Defaults to None.
                                   If port is None, the class will try to auto detect the port.
-            xonxoff (bool, optional): Enables or disables software flow control (XON/XOFF). Defaults to True.
             baudrate (int, optional): The baud rate for the serial connection. Defaults to 115200.
         """
         self.__serial = None
         self.__port = port
-        self.__xonxoff = xonxoff
         self.__baudrate = baudrate
 
 
@@ -247,7 +243,7 @@ class SerialProtocol(TransportProtocol):
                 detected = await protocol.detect_device()
                 return port_name if detected else None
             finally:
-                protocol.close()
+                await protocol.close()
 
         # Run all detections concurrently
         tasks = [detect_on_port(port) for port in valid_ports]
@@ -267,7 +263,7 @@ class SerialProtocol(TransportProtocol):
         Raises:
             RuntimeError: If the NV200 device cannot be detected or connected to.
         """
-        self.__serial = aioserial.AioSerial(port=self.__port, xonxoff=self.__xonxoff, baudrate=self.__baudrate)
+        self.__serial = aioserial.AioSerial(port=self.__port, xonxoff=False, baudrate=self.__baudrate)
         if self.__port is None:
             self.__port = await self.detect_port()
         if self.__port is None:
@@ -278,7 +274,8 @@ class SerialProtocol(TransportProtocol):
         await self.__serial.write_async(cmd.encode('utf-8'))
 
     async def read_response(self) -> str:
-        return await self.__serial.readline_async()
+        data = await self.__serial.read_until_async(serial.XON)
+        return data.replace(b'\x11', b'').replace(b'\x13', b'') # strip XON and XOFF characters
 
     async def close(self):
         if self.__serial:
